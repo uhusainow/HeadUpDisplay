1e85502b4e969929b6948147c47365f038cff376
uhusainow <uhusainow@student.tgm.ac.at>
2020-12-20 14:41:54 +0100

    BluetoothConnection

--- app/src/main/java/at/ac/tgm/hit/uhusainow/headupdisplay/bluetooth/BluetoothConnection.java
+++ app/src/main/java/at/ac/tgm/hit/uhusainow/headupdisplay/bluetooth/BluetoothConnection.java
@@ -32,7 +32,8 @@ public class BluetoothConnection implements Serializable {
                     //System.out.println("DEVICE: " + bondedDevice.getName());
                     if(bondedDevice.getName().equals(BluetoothConnection.BLEUTOOTH_DEVICE) ||
                             bondedDevice.getName().equals("CAN OBDII") ||
-                            bondedDevice.getName().equals("CBT")){
+                            bondedDevice.getName().equals("CBT") ||
+                            bondedDevice.getName().equals("OBDII")){
                         this.bluetoothSocket = bondedDevice.createRfcommSocketToServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"));
                         this.bluetoothSocket.connect();
                         break;
